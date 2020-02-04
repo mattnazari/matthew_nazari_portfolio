07fecac4ac466f29ecdf0f08ed0716be1b717117
mattnazari <matthew.nazari8@gmail.com>
2020-02-03 19:45:09 -0800

    scss setup / header comp v01

--- src/comps/Header/index.js
+++ src/comps/Header/index.js
@@ -2,8 +2,14 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div>
-      <h1>hello</h1>
+    <div className="headerContainer">
+      <div className="logo"></div>
+      <div className="navItemsContainer">
+        <div className="navItems projects">Projects</div>
+        <div className="navItems about">About</div>
+        <div className="navItems skills">Skills</div>
+        <div className="navItems contact">Contact</div>
+      </div>
     </div>
   );
 };
