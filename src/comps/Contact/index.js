cd1e7580f6c1daba23e788858da164ad2306a613
mattnazari <matthew.nazari8@gmail.com>
2020-02-09 03:54:15 -0800

    contact finished

--- src/comps/Contact/index.js
+++ src/comps/Contact/index.js
@@ -7,7 +7,57 @@ const Contact = () => {
     <div>
       <Header />
       <section className="contactCont">
-        <div></div>
+        <div className="projectTitleCont">
+          <div className="underlineCont">
+            <div className="projectsTitle">Contact Me</div>
+            <div className="blackunderline"></div>
+          </div>
+        </div>
+        <div className="contactIconsCont">
+          <div className="circleIcons">
+            <div className="contactIconsCont">
+              <a
+                href="https://github.com/mattnazari"
+                target="_blank"
+                className="contactAnch githubAnch"
+              >
+                <div className="contactIcons contactGithub"></div>
+                <p className="contactIconsText">
+                  https://github.com/mattnazari
+                </p>
+              </a>
+            </div>
+            <div className="contactIconsCont">
+              <a href="tel:604-562-3324" className="contactAnch phoneAnch">
+                <div className="contactIcons contactPhone"></div>
+                <p className="contactIconsText">604-562-3324</p>
+              </a>
+            </div>
+          </div>
+          <div className="squareIcons">
+            <div className="contactIconsCont">
+              <a
+                href="mailto:matthewnaz8@gmail.com"
+                className="contactAnch emailAnch"
+              >
+                <div className="contactIcons contactEmail"></div>
+                <p className="contactIconsText">matthewnaz8@gmail.com</p>
+              </a>
+            </div>
+            <div className="contactIconsCont">
+              <a
+                href="https://www.linkedin.com/in/matthew-nazari/"
+                target="_blank"
+                className="contactAnch linkedinAnch"
+              >
+                <div className="contactIcons contactLinkedin"></div>
+                <p className="contactIconsText">
+                  https://www.linkedin.com/in/matthew-nazari/
+                </p>
+              </a>
+            </div>
+          </div>
+        </div>
       </section>
       <Footer />
     </div>
